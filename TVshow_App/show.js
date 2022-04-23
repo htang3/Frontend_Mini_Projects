c2173b4a79e5d9d2fdc14a2999c16e5cc32ecf6d
Hung Q Tang <tangquochung2901@gmail.com>
2022-04-23 19:22:00 +0800

    fixed data loading and applied style

--- TVshow_App/show.js
+++ TVshow_App/show.js
@@ -1,4 +1,4 @@
-const mainctn = document.querySelector("div");
+const mainctn = document.querySelector("#main");
 const input = document.querySelector("#input");
 
 const form = document.querySelector("form");
@@ -9,35 +9,43 @@ const SEARCH_API = `https://api.tvmaze.com/search/shows?q=`;
 const getTvShow = async (url) => {
   const initialShowDisplayData = await axios.get(url);
   console.log(initialShowDisplayData.data);
-  await makeShowCard(initialShowDisplayData.data);
+  makeShowCard(initialShowDisplayData.data);
 };
-getTvShow(INITIAL_API);
-const makeShowCard = (arrayOfShows) => {``
+
+const makeShowCard = async (arrayOfShows) => {
   mainctn.innerHTML = "";
   arrayOfShows.forEach((eachShow) => {
     const img = eachShow.show.image;
     const rating = eachShow.show.rating.average;
     const name = eachShow.show.name;
-    if (img && rating && name) {
-      console.log("here");
-      mainctn.innerHTML = `
-      <div>
+    const link = eachShow.show.officialSite;
+    const div = document.createElement("div");
+    console.log(link);
+    if (img != null && rating != null && name != null) {
+      div.classList.add("show");
+      div.innerHTML = `
         <img src=${img.medium} />
-        <div>
+        <div class="cardText">
         <h4>${name}</h4>
         <span>${rating}</span>
         </div>
-      </div>
-      
+        <div class="website">
+        <h5>Watch the <a href="${link}">${name}</a></h5>
+          
+        </div>
       `;
+      console.log("end here");
+    } else {
+      div.innerHTML = ``;
     }
-
+    mainctn.append(div);
   });
 };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchTerm = input.value;
-  getTvShow(SEARCH_API + `${searchTerm}`);
-  searchTerm.value = "";
+  getTvShow(SEARCH_API + searchTerm);
+  input.value = "";
+  input.autofocus = true;
 });
